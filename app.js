2d558c065e0b4b3e5a9470df444570297e54617e
xiaoyc-9364 <739759506@qq.com>
2018-01-18 17:50:29 +0800

    add publish page

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var http = require('http');
 var querystring = require('querystring');
 var fs = require('fs');
 
+var staticRequeat = require('./routers/staticRequeat'); //静态请求
+
 mongoose.connect('mongodb://localhost:27019/blog', {useMongoClient: true}, function(err) {
     if (err) {
         console.log('数据库连接失败!');
@@ -11,53 +13,25 @@ mongoose.connect('mongodb://localhost:27019/blog', {useMongoClient: true}, funct
     }
 });
 
-function getCss(req, res) {
-    if (/^(\/public\/)\w+(\.css)$/.test(req.url) ) {
-        fs.readFile(`.${req.url}`, function(err, data) {
-                res.writeHead(200, {'Content-Type': 'text/css; charset=utf-8'});
-                res.write(data);
-                res.end();
-            });
-            // return;
-    }
-}
-
-
-
 var server = http.createServer(function(req, res) {
-    if(req.method === 'POST') {
-        let body = '';
-        req.on('data', (data) => {
-            console.info(data);
-            body += data;
-        });
+    // if(req.method === 'POST') {
+    //     let body = '';
+    //     req.on('data', (data) => {
+    //         console.info(data);
+    //         body += data;
+    //     });
     
-        req.on('end', (data) => {
-            body = querystring.parse(body);
-            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-            res.write(`<h1>添加文章${body.title}成功!</h1>`);
-            res.end();
-        });
+    //     req.on('end', (data) => {
+    //         body = querystring.parse(body);
+    //         res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    //         res.write(`<h1>添加文章${body.title}成功!</h1>`);
+    //         res.end();
+    //     });
 
-        return;
-    } else {
-        // if (/\.css$/.test(req.url)) {
-        //     fs.readFile(`./${req.url}`, function(err,data) {
-        //         res.writeHead(200, {'Content-Type': 'text/css; charset=utf-8'});
-        //         res.write(data);
-        //         res.end();
-        //     });
-        //     return;
-        // }
-        // getCss(req, res);
-        if (/^\/public\/\w+\.css$/.test(req.url) ) {
-            fs.readFile(`./${req.url}`, function(err, data) {
-                    res.writeHead(200, {'Content-Type': 'text/css; charset=utf-8'});
-                    res.write(data);
-                    res.end();
-                });
-                return;
-        }
+    //     return;
+    // } else {
+        staticRequeat.getCss(req, res);
+        staticRequeat.getJavascript(req, res);
         switch(req.url) {
             case '/': 
                 fs.readFile('./views/main/index.html', function(err,data) {
@@ -67,8 +41,8 @@ var server = http.createServer(function(req, res) {
                 });
             break;
             
-            case '/about': 
-                fs.readFile('./views/main/about.html', function(err,data) {
+            case '/publish': 
+                fs.readFile('./views/main/publish.html', function(err,data) {
                     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
                     res.write(data);
                     res.end();
@@ -78,14 +52,16 @@ var server = http.createServer(function(req, res) {
             case '/favicon.ico': 
                 res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
                 res.write('xx');
+                
                 res.end();
             break;
     
             default:
-                res.end();
+                // res.end();
                 
         }
-    }
+
+    // }
 });
 server.listen(8088, 'localhost');
-console.log(new Date);
+console.log(new Date().getHours()+'点' + new Date().getMinutes() + '分');
