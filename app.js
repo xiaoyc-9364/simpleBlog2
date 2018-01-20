ea2b93ecaa9682aa177ff53a5119aa23b6443e18
xiaoyc-9364 <739759506@qq.com>
2018-01-20 16:25:00 +0800

    add read blog and watch sameone blog list

--- app.js
+++ app.js
@@ -1,52 +1,35 @@
-var mongoose = require('mongoose');
-var http = require('http');
-var querystring = require('querystring');
-var fs = require('fs');
-var blogData = require('./models/Blog');
-var staticRequeat = require('./routers/staticRequeat'); //静态请求
+const mongoose = require('mongoose'),
+    http = require('http'),
+    querystring = require('querystring'),
+    fs = require('fs'),
 
+//自定义模块
+const blogData = require('./models/Blog'),
+    showIndex = require('./models/showIndex'),
+    readBlog = require('./models/readBlog'),
+    gotoPublish = require('./models/gotoPublish'),
+    favicon = require('./models/favicon'),
+    staticRequeat = require('./routers/staticRequeat'); //静态请求
+
+//连接数据库
 mongoose.connect('mongodb://localhost:27019/blog', {useMongoClient: true}, function(err) {
     if (err) {
         console.log('数据库连接失败!');
     } else {
-        console.log('数据库连接成!');
+        console.log(new Date().getHours()+'点' + new Date().getMinutes() + '分,数据库连接成!');
     }
 });
-
-var server = http.createServer(function(req, res) {
+//创建服务器
+const server = http.createServer(function(req, res) {
     if(req.method === 'POST') {
         blogData(req, res);
     } else {
-        staticRequeat.getCss(req, res);
-        staticRequeat.getJavascript(req, res);
-        switch(req.url) {
-            case '/': 
-                fs.readFile('./views/main/index.html', function(err,data) {
-                    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-                    res.write(data);
-                    res.end();
-                });
-            break;
-            
-            case '/publish': 
-                fs.readFile('./views/main/publish.html', function(err,data) {
-                    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-                    res.write(data);
-                    res.end();
-                });
-            break;
-    
-            case '/favicon.ico': 
-                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-                res.end();
-            break;
-    
-            default:
-                // res.end();
-                
-        }
-
+        staticRequeat.getCss(req, res);     //css请求
+        staticRequeat.getJavascript(req, res);      //js请求
+        readBlog(req, res);     //阅读博客文章请求
+        showIndex(req, res);    //首页请求
+        gotoPublish(req, res);  //发布博客页面请求
+        favicon(req, res);      //图标请求
     }
 });
 server.listen(8088, 'localhost');
-console.log(new Date().getHours()+'点' + new Date().getMinutes() + '分');
